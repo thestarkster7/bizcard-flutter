49396d187d2a61a3c8c92d890c6aa447d23e934e
Abhishek Manjanwal <55803624+thestarkster7@users.noreply.github.com>
2021-06-15 15:40:44 +0530

    added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 void main() {
   runApp(MyApp());
@@ -12,78 +13,94 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.indigo,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/abhishek.jpg'),
-              ),
-              Text(
-                "Abhishek Manjanwal",
-                style: TextStyle(
-                  fontFamily: "PoiretOne",
-                  fontSize: 32.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/abhishek.jpg'),
                 ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                "COMPUTER SCIENCE JUNIOR",
-                style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontSize: 16.0,
-                  letterSpacing: 2.5,
-                  color: Colors.indigo.shade100,
+                Text(
+                  "Abhishek Manjanwal",
+                  style: TextStyle(
+                    fontFamily: "PoiretOne",
+                    fontSize: 32.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.indigo.shade100,
+                SizedBox(
+                  height: 5.0,
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
+                Text(
+                  "COMPUTER SCIENCE JUNIOR",
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontSize: 16.0,
+                    letterSpacing: 2.5,
+                    color: Colors.indigo.shade100,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.indigo.shade100,
+                  ),
+                ),
+                TextButton.icon(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    UrlLauncher.launch("tel://9999999999");
+                    print("Phone Pressed");
+                  },
+                  icon: Icon(
                     Icons.phone,
                     color: Colors.indigo,
                   ),
-                  title: Text(
+                  label: Text(
                     "+91 999 999 9999",
                     style: TextStyle(
                       fontFamily: 'Roboto',
-                      color: Colors.teal.shade900,
+                      color: Colors.indigo.shade900,
                       fontSize: 20,
                     ),
                   ),
                 ),
-              ),
-              Card(
-                  color: Colors.white,
-                  margin:
+                SizedBox(
+                  height: 5.0,
+                ),
+                TextButton.icon(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    padding: MaterialStateProperty.all(
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.indigo,
                     ),
-                    title: Text(
-                      "amanjanwal@gmail.com",
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        color: Colors.teal.shade900,
-                        fontSize: 20,
-                      ),
+                  ),
+                  onPressed: () {
+                    print("Email Pressed");
+                  },
+                  icon: Icon(
+                    Icons.email,
+                    color: Colors.indigo,
+                  ),
+                  label: Text(
+                    "amanjanwal@gmail.com",
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      color: Colors.indigo.shade900,
+                      fontSize: 20,
                     ),
-                  )),
-            ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
